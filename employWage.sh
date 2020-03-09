1bb00c16e604ebf5be482b0b42288e0ed51062b8
AswathiMohan94 <aswathimohantly@gmail.com>
2020-03-09 19:00:42 +0530

    [Aswathi] add.adding attendence

--- employWage.sh
+++ employWage.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
 echo "welcome"
+isPresent=1;
+isAbsent=2;
+WagePerHr=20;
+empCheck=$((RANDOM%2));
+if [ $isPresent -eq $empCheck ]
+then
+	TotalHr=8;
+else
+	TotalHr=0;
+fi
+Wage=$(($WagePerHr*$TotalHr));
+
